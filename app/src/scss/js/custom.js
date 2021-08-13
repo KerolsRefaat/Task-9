c887f9a1c3d8ea77ad990420753f44f7411277dd
jeorji <george_19955@yahoo.com>
2021-08-14 01:48:45 +0200

    second commit

---
+++ app/src/scss/js/custom.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+    var btn = document.querySelector('header .mycontainer .navbar-toggler');
+    var menu =  document.querySelector('header .mycontainer .navbar-toggler span');
+   
+    console.log(btn);
+    console.log("test menu" + menu);
+    console.log("test11");
+   
+    menu.click( function(){
+   
+      this.css({'background-img:'})
+   
+    }
+        
+       
+   
+       //  menu.classList.toggle('.close');
+   //      console.log(menu);
+   //      console.log("test btn" + btn);
+   //  console.log("test menu" + menu);
+        
+    )
+   
+   });
+   
